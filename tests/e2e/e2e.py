239f967ded77af6d016a8e43e156e19ae9361f8f
7db9a <>
2024-02-06 12:51:31 -0500

    tests(e2e): assert user is not found if not created.

--- tests/e2e/e2e.py
+++ tests/e2e/e2e.py
@@ -9,11 +9,11 @@ class TestCLIApp(unittest.TestCase):
         stdout, stderr = process.communicate()
         return stdout.decode('utf-8'), stderr.decode('utf-8'), process.returncode
 
-    def test_create_user(self):
-        command = "poetry run tsrc-cli create-user test_user_id test_user_name test_password"
-        stdout, stderr, exit_code = self.run_cli_command(command)
-        self.assertEqual(exit_code, 0)
-        self.assertIn("User 'test_user_name' with ID 'test_user_id' created successfully", stdout)
+    #def test_create_user(self):
+    #    command = "poetry run tsrc-cli create-user test_user_id test_user_name test_password"
+    #    stdout, stderr, exit_code = self.run_cli_command(command)
+    #    self.assertEqual(exit_code, 0)
+    #    self.assertIn("User 'test_user_name' with ID 'test_user_id' created successfully", stdout)
 
     def test_get_user(self):
         # Assuming the user 'test_user_id' is already created
@@ -21,6 +21,13 @@ class TestCLIApp(unittest.TestCase):
         stdout, stderr, exit_code = self.run_cli_command(command)
         self.assertEqual(exit_code, 0)
         self.assertIn("User 'test_user_name' with ID 'test_user_id' retrieved successfully", stdout)
+
+    def test_get_user(self):
+        # Assuming the user 'test_user_id' is not created
+        command = "poetry run tsrc-cli get-user test_user_id_non_existant"
+        stdout, stderr, exit_code = self.run_cli_command(command)
+        self.assertEqual(exit_code, 0)
+        self.assertIn("Failed to retrieve user: User not found\n", stdout)
 
 if __name__ == '__main__':
     unittest.main()
