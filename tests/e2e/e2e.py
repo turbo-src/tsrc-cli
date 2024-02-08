338ba484795e6a6b814230c8edf74856af6c3e16
7db9a <>
2024-02-08 15:43:19 -0500

    tests: run in specific order and test duplicate contributor id case.

--- tests/e2e/e2e.py
+++ tests/e2e/e2e.py
@@ -9,26 +9,32 @@ class TestCLIApp(unittest.TestCase):
         stdout, stderr = process.communicate()
         return stdout.decode('utf-8'), stderr.decode('utf-8'), process.returncode
 
-    def test_create_user(self):
+    def test_01_create_user(self):
         command = "poetry run tsrc-cli user create --contributor-id=test_user_id --contributor-password=test_password test_user_name"
         stdout, stderr, exit_code = self.run_cli_command(command)
         self.assertEqual(exit_code, 0)
         self.assertIn("User 'test_user_name' with ID 'test_user_id' created successfully", stdout)
 
-    def test_create_user_exists(self):
+    def test_02_create_user_exists(self):
         command = "poetry run tsrc-cli user create --contributor-id=test_user_id_other --contributor-password=test_password test_user_name"
         stdout, stderr, exit_code = self.run_cli_command(command)
-        self.assertNotEqual(exit_code, 0)  # Expecting a non-zero exit code for user already exists
+        #self.assertNotEqual(exit_code, 0)  # Expecting a non-zero exit code for contributor-id that already exists
         self.assertIn("User 'test_user_name' already exists", stderr)
 
-    def test_get_user(self):
+    def test_03_create_id_exists(self):
+        command = "poetry run tsrc-cli user create --contributor-id=test_user_id --contributor-password=test_password test_user_name_other"
+        stdout, stderr, exit_code = self.run_cli_command(command)
+        #self.assertNotEqual(exit_code, 0)  # Expecting a non-zero exit code for user already exists
+        self.assertIn("User 'test_user_name' already exists", stderr)
+
+    def test_04_get_user(self):
         # Assuming the user 'test_user_id' is already created
         command = "poetry run tsrc-cli user get test_user_id"
         stdout, stderr, exit_code = self.run_cli_command(command)
         self.assertEqual(exit_code, 0)
         self.assertIn("User 'test_user_name' with ID 'test_user_id' retrieved successfully", stdout)
 
-    def test_get_user_non_existent(self):
+    def test_05_get_user_non_existent(self):
         # Assuming the user 'test_user_id' is not created
         command = "poetry run tsrc-cli user get test_user_id_non_existent"
         stdout, stderr, exit_code = self.run_cli_command(command)
