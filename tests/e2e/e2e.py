46c24f62576c9bf139c31ccbef46daa06147c56f
7db9a <>
2024-02-08 10:36:37 -0500

    tests(e2e): update given new syntax, etc.

--- tests/e2e/e2e.py
+++ tests/e2e/e2e.py
@@ -10,31 +10,30 @@ class TestCLIApp(unittest.TestCase):
         return stdout.decode('utf-8'), stderr.decode('utf-8'), process.returncode
 
     def test_create_user(self):
-        command = "poetry run tsrc-cli create-user test_user_id test_user_name test_password"
+        command = "poetry run tsrc-cli user create --contributor-id=test_user_id --contributor-password=test_password test_user_name"
         stdout, stderr, exit_code = self.run_cli_command(command)
         self.assertEqual(exit_code, 0)
         self.assertIn("User 'test_user_name' with ID 'test_user_id' created successfully", stdout)
 
     def test_create_user_exists(self):
-        command = "poetry run tsrc-cli create-user test_user_id test_user_name test_password"
+        command = "poetry run tsrc-cli user create --contributor-id=test_user_id_other --contributor-password=test_password test_user_name"
         stdout, stderr, exit_code = self.run_cli_command(command)
         self.assertNotEqual(exit_code, 0)  # Expecting a non-zero exit code for user already exists
-        self.assertIn("User, test_user_name, already exists", stderr)
+        self.assertIn("User 'test_user_name' already exists", stderr)
 
     def test_get_user(self):
         # Assuming the user 'test_user_id' is already created
-        command = "poetry run tsrc-cli get-user test_user_id"
+        command = "poetry run tsrc-cli user get test_user_id"
         stdout, stderr, exit_code = self.run_cli_command(command)
         self.assertEqual(exit_code, 0)
         self.assertIn("User 'test_user_name' with ID 'test_user_id' retrieved successfully", stdout)
 
     def test_get_user_non_existent(self):
         # Assuming the user 'test_user_id' is not created
-        command = "poetry run tsrc-cli get-user test_user_id_non_existent"
+        command = "poetry run tsrc-cli user get test_user_id_non_existent"
         stdout, stderr, exit_code = self.run_cli_command(command)
         self.assertNotEqual(exit_code, 0)  # Expecting a non-zero exit code for non-existent user
         self.assertIn("User not found", stderr)
 
 if __name__ == '__main__':
     unittest.main()
-
